c29cf3c82a2825674abeded04d95604526c51458
dvdhutch <davidmhutchinson@hotmail.com>
2024-08-25 22:47:33 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -27,11 +27,6 @@ export default function Home({ allPostsData }) {
         <p> <b> What important truth do very few people agree with you on? Or, to the entrepreneur, what valuable company is nobody building? <a href="https://en.wikipedia.org/wiki/Zero_to_One">(<i>Thiel</i>)</a> </b> </p>
         <p> Without fail, this question leads me to ponder, as I try to understand and piece together solutions to problems. Outwardly people describe me as calm and affable but inwardly I can't sit still and am always trying to figure out what comes next.</p>
         <p> Some of the things I'm most interested in include education technology, and in particular, the role of <a href="https://www.ted.com/talks/sal_khan_the_amazing_ai_super_tutor_for_students_and_teachers/c">artificial intelligence</a> in the future of education, the <a href="http://paulgraham.com/cities.html">stories</a> cities tell, <a href="https://ourworldindata.org/">visualizing</a> human geography, particularly in the context of highlighting the <a href="https://www.segregationbydesign.com/">failure of urban renewal projects</a> and advocating for continued investment in large, sustainable <a href="https://www.noahpinion.blog/p/interview-patrick-collison-co-founder">infrastructure projects</a> in America, and by extension the world. </p>
-        <p> I am an incoming graduate student at <a href="https://www.tc.columbia.edu">Teachers College, Columbia University</a> studying instructional technology and am currently working at <a href="https://www.las.ch">Leysin American School</a> in Switzerland.</p>
-        <p> Previously, I worked as a Product Manager at <a href="https://www.liveohana.ai">Ohana</a>, where I led early growth in New York City. I also taught at <a href="https://www.goprojectnyc.org">GO Project</a>, an NYC based non-profit serving students from public schools across the city. </p>
-        <p> Before that, I started a startup, <a href="https://www.suoni.co">Suoni</a>, where I worked on making AI-generated lesson plans for teachers -- and learned a lot along the way.</p>
-        <p> I am an alumnus of the <a href="https://www.shc.psu.edu">Penn State Schreyer Honors College</a> and <a href="https://www.academy.psu.edu">Presidential Leadership Academy at Penn State</a>.
-        </p>
       </section>
 
       <section className={utilStyles.headingMd}>
