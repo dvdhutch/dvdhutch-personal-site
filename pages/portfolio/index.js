e9ab6d8d01bdc583d70a4ed93fc89a1c09236c6d
dvdhutch <davidmhutchinson@hotmail.com>
2024-10-15 16:08:28 -0400

    revert css change

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -44,11 +44,7 @@ export default function Portfolio() {
           {projects.map((project, index) => (
             <Link key={index} href={`/portfolio/${project.slug}`}>
               <div className={styles.card}>
-                <img 
-                  src={project.imageUrl} 
-                  alt={project.title} 
-                  className={`${styles.image} ${styles.portfolioRoundedImage}`} 
-                />
+                <img src={project.imageUrl} alt={project.title} className={styles.image} />
                 <h3>{project.title}</h3>
                 <p>{project.description}</p>
               </div>
