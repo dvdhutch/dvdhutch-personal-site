5b447abe959b0d0c2419fb1df22c4b7390588327
dvdhutch <davidmhutchinson@hotmail.com>
2024-10-15 17:24:40 -0400

    Update [slug].js

--- pages/portfolio/[slug].js
+++ pages/portfolio/[slug].js
@@ -31,7 +31,7 @@ const projectData = {
       <>
         <h1>Student Perceptions of Digital Learning Technology: A Cross-Cultural Analysis of Music Students</h1>
         <p><i>This project was advised by <a href="https://arts.psu.edu/directory/jacob-holster" target="_blank">Dr. Jacob Holster</a>.</i></p>
-        <p><i>I presented this project at the New York State School Music Association Winter Conference (Rochester, NY) and the Penn State Undergraduate Expo (University Park, PA), as well as the Texas Music Educators Association/Technology In Music Education Conference (San Antonio, TX). In 2024 I won the Pennsylvania Music Educators Association Award for Excellence in Research.</i></p>
+        <p><i>I presented this project at the <a href="https://www.nyssma.org/" target="_blank">New York State School Music Association</a> Winter Conference (Rochester, NY), the <a href="https://urfm.psu.edu/programs/undergraduate-exhibition" target="_blank">Penn State Undergraduate Expo</a> (University Park, PA), and the <a href="https://www.tmea.org/" target="_blank">Texas Music Educators Association</a>/<a href="https://ti-me.org/" target="_blank">Technology In Music Education</a> Conference (San Antonio, TX). In 2024 I won the <a href="https://www.pmea.net/" target="_blank">Pennsylvania Music Educators Association</a> Award for Excellence in Research.</i></p>
         <p><i>This is the presentation-ready version of a full-length research paper. Read the whole thing <a href="https://honors.libraries.psu.edu/catalog/9008dmh6286" target="_blank">here</a>!</i></p>
         <h2>Objective & Hypotheses</h2>
         <ul>
