4c32d78481840e433ff46b7f1d392bc863dec30f
dvdhutch <davidmhutchinson@hotmail.com>
2024-10-25 13:41:33 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home({ allPostsData }) {
             <span className={utilStyles.arrow}>▶</span> 
             <span className={utilStyles.indentedText}>First product hire at <Link href="https://liveohana.ai/"><a>Ohana</a></Link>&mdash;built the <Link href="https://liveohana.ai/blog/ohana-neo"><a>company blog</a></Link> and early iterations of the <Link href="https://liveohana.ai/sublet/new-york-city"><a>neighborhood information tool</a></Link></span>
           </li>
-          <li>
+          <li className={utilStyles.workItem}>
             <span className={utilStyles.arrow}>▶</span>
             <span className={utilStyles.indentedText}>Zankel Fellow at <Link href="https://liveohana.ai/"><a>Columbia University</a></Link>&mdash;I teach kids how to code robots and advise an <Link href="https://www.firstlegoleague.org/"><a>FLL team</a></Link></span>
           </li>
