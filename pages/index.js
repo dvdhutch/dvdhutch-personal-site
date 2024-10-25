c723377a90fef5b1bf34a03b7a3542034e51ec97
dvdhutch <davidmhutchinson@hotmail.com>
2024-10-25 13:39:08 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -30,14 +30,12 @@ export default function Home({ allPostsData }) {
         <h2>Experience</h2>
         <ul className={utilStyles.arrowList}>
           <li className={utilStyles.workItem}>
-            <div>
-              <span className={utilStyles.arrow}>▶</span> 
-              <span className={utilStyles.indentedText}>First product hire at <Link href="https://liveohana.ai/"><a>Ohana</a></Link>&mdash;built the <Link href="https://liveohana.ai/blog/ohana-neo"><a>company blog</a></Link> and early iterations of the <Link href="https://liveohana.ai/sublet/new-york-city"><a>neighborhood information tool</a></Link></span>
-            </div>
-            <div>
-              <span className={utilStyles.arrow}>▶</span>
-              <span className={utilStyles.indentedText}>Zankel Fellow at <Link href="https://liveohana.ai/"><a>Columbia University</a></Link>&mdash;I teach kids how to code robots and advise an <Link href="https://www.firstlegoleague.org/"><a>FLL team</a></Link></span>
-            </div>
+            <span className={utilStyles.arrow}>▶</span> 
+            <span className={utilStyles.indentedText}>First product hire at <Link href="https://liveohana.ai/"><a>Ohana</a></Link>&mdash;built the <Link href="https://liveohana.ai/blog/ohana-neo"><a>company blog</a></Link> and early iterations of the <Link href="https://liveohana.ai/sublet/new-york-city"><a>neighborhood information tool</a></Link></span>
+          </li>
+          <li>
+            <span className={utilStyles.arrow}>▶</span>
+            <span className={utilStyles.indentedText}>Zankel Fellow at <Link href="https://liveohana.ai/"><a>Columbia University</a></Link>&mdash;I teach kids how to code robots and advise an <Link href="https://www.firstlegoleague.org/"><a>FLL team</a></Link></span>
           </li>
         </ul>
       </section>
