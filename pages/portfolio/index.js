91cae8a86c980f0b032a1a7d44f6fbe39249931b
dvdhutch <davidmhutchinson@hotmail.com>
2024-12-04 11:19:14 -0500

    Update index.js

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -8,6 +8,12 @@ import Link from 'next/link';
 export default function Portfolio() {
   const projects = [
     {
+      title: 'Student Perceptions of Digital Learning Technology: A Cross-Cultural Analysis of Music Students',
+      description: 'A robust analysis of music student\'s perceptions of digital learning technology at the Penn State School of Music and University of Oxford Faculty of Music.',
+      imageUrl: 'https://media.architecturaldigest.com/photos/563000c2bf2db2f83856e12c/16:9/w_3040,h_1710,c_limit/harry-potter-sets-christ-church-college-oxford15.jpg',
+      slug: 'thesis',
+    },
+    {
       title: 'NYC\'s Underserved Neighborhoods',
       description: 'A deep-dive into New York neighborhoods that are underserved by the New York City Subway.',
       imageUrl: '/images/subway.jpg',
@@ -24,12 +30,6 @@ export default function Portfolio() {
       description: 'Geography is deterministic. For some, geography is destiny. For others, geography is fate. For Williamsport,a moderately large city situated on the West Branch of the Susquehanna River, as well as the principal cultural, economical, and commercial node of Central Pennsylvania, geography tells the story of of how and why a modest village became the wealthiest place in the world.',
       imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi6VA6d2Qd4dsPoS7kCXeAyqaXZVBBZWVQj83AYia51Tj-Nso-naTeo-BUotriI_HZ1lZfUrj9C90BM0Dzy-U_pGuotiLBE7B05zP9TXvKQlBSSBFpnrTJ-MDzmRifvmJCUo_oF24ioMfeACC7bRpJdN5haerID8gZQIEFKK9FjpKjfQBgdyy0cHlzFRg/s1000/labeled%20williamsport.jpg',
       slug: 'williamsport',
-    },
-    {
-      title: 'Student Perceptions of Digital Learning Technology: A Cross-Cultural Analysis of Music Students',
-      description: 'A robust analysis of music student\'s perceptions of digital learning technology at the Penn State School of Music and University of Oxford Faculty of Music.',
-      imageUrl: 'https://media.architecturaldigest.com/photos/563000c2bf2db2f83856e12c/16:9/w_3040,h_1710,c_limit/harry-potter-sets-christ-church-college-oxford15.jpg',
-      slug: 'thesis',
     },
   ];
 
