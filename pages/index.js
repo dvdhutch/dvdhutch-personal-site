acdb374fd2cb0206b4797a72c5ca5ec6efe35c21
dvdhutch <davidmhutchinson@hotmail.com>
2024-12-04 13:05:49 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -59,7 +59,7 @@ export default function Home({ allPostsData }) {
           <li className={utilStyles.workItem}>
             <span className={utilStyles.arrow}>▶</span>
             <span className={utilStyles.indentedText}><a href="https://www.shc.psu.edu" target="_blank">Penn State Schreyer Honors College</a></span>
-            <span className={utilStyles.workDate}>     2020-2024</span>
+            <span className={utilStyles.workDate}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2020-2024</span>
           </li>
           <li className={utilStyles.workItem}>
             <span className={utilStyles.arrow}>▶</span>
