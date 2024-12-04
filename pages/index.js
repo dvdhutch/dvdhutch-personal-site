09688763e2efc74f1da37d63fff2d331c6dad1d6
dvdhutch <davidmhutchinson@hotmail.com>
2024-12-04 12:42:04 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,27 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I live in New York and am an MA candidate at <Link href="https://columbia.edu"><a target="_blank">Columbia University</a></Link>, where I'm involved . Before that, I've had a number of experiences building and teaching, but always invested in <Link href="https://map.simonsarris.com/p/careful-technology"><a>beautiful craft</a></Link>, <Link href="https://x.com/McConaughey/status/1822981269099028696"><a target="_blank">good design</a></Link>, and <Link href="https://linkedin.com/in/dvdhutch/#volunteering_experience"><a target="_blank">helping others</a></Link>.</p>
+        <p>I live in New York and am an MA candidate at <Link href="https://columbia.edu"><a target="_blank">Columbia University</a></Link>, where I'm involved in the <Link href="https://www.snowdaylearninglab.org/team"><a target="_blank">Snow Day Learning Lab</a></Link> and <Link href="https://tltlab.org/"><a target="_blank">Transformative Learning Technologies Lab</a></Link>. Before that, I've had a number of experiences building and teaching, and always invested in <Link href="https://map.simonsarris.com/p/careful-technology"><a target="_blank">beautiful craft</a></Link>, <Link href="https://x.com/McConaughey/status/1822981269099028696"><a target="_blank">good design</a></Link>, and <Link href="https://linkedin.com/in/dvdhutch/#volunteering_experience"><a target="_blank">helping others</a></Link>.</p>
+        <h3>Interests</h3>
+        <ul className={utilStyles.arrowList}>
+          <li className={utilStyles.workItem}>
+            <span className={utilStyles.arrow}>▶</span>
+            <span className={`${utilStyles.lightTextColor} ${utilStyles.indentedText}`}>The role of <a href="https://www.ted.com/talks/sal_khan_the_amazing_ai_super_tutor_for_students_and_teachers/c">artificial intelligence</a> in the future of education</span>
+          </li>
+          <li className={utilStyles.workItem}>
+            <span className={utilStyles.arrow}>▶</span>
+            <span className={`${utilStyles.lightTextColor} ${utilStyles.indentedText}`}>The <a href="http://paulgraham.com/cities.html">stories</a> cities tell</span>
+          </li>
+          <li className={utilStyles.workItem}>
+            <span className={utilStyles.arrow}>▶</span>
+            <span className={`${utilStyles.lightTextColor} ${utilStyles.indentedText}`}><a href="https://www.noahpinion.blog/p/interview-patrick-collison-co-founder">American Dynamism</a></span>
+          </li>
+          <li className={utilStyles.workItem}>
+            <span className={utilStyles.arrow}>▶</span>
+            <span className={`${utilStyles.lightTextColor} ${utilStyles.indentedText}`}><a href="https://sites.psu.edu/academy/2023/12/03/tales-of-water-and-mountains/">Early American History</a></span>
+          </li>
+        </ul>
+        <h3>Experience</h3>
         <ul className={utilStyles.arrowList}>
           <li className={utilStyles.workItem}>
             <span className={utilStyles.arrow}>▶</span>
@@ -45,46 +65,27 @@ export default function Home({ allPostsData }) {
             <span className={`${utilStyles.lightTextColor} ${utilStyles.indentedTextLight}`}>Built the <Link href="https://www.dhutch.fyi/portfolio/ohana_blog"><a>company blog</a></Link> and the <Link href="https://www.dhutch.fyi/portfolio/ohana_neighborhood"><a>neighborhood information tool</a></Link></span>
           </li>
         </ul>
-        <ul className={utilStyles.arrowList}>
-          <li className={utilStyles.workItem}>
-            <span className={utilStyles.arrow}>▶</span>
-            <span className={`${utilStyles.lightTextColor} ${utilStyles.indentedText}`}>The role of <a href="https://www.ted.com/talks/sal_khan_the_amazing_ai_super_tutor_for_students_and_teachers/c">artificial intelligence</a> in the future of education</span>
-          </li>
-          <li className={utilStyles.workItem}>
-            <span className={utilStyles.arrow}>▶</span>
-            <span className={`${utilStyles.lightTextColor} ${utilStyles.indentedText}`}>The <a href="http://paulgraham.com/cities.html">stories</a> cities tell</span>
-          </li>
-          <li className={utilStyles.workItem}>
-            <span className={utilStyles.arrow}>▶</span>
-            <span className={`${utilStyles.lightTextColor} ${utilStyles.indentedText}`}><a href="https://www.noahpinion.blog/p/interview-patrick-collison-co-founder">American Dynamism</a></span>
-          </li>
-          <li className={utilStyles.workItem}>
-            <span className={utilStyles.arrow}>▶</span>
-            <span className={`${utilStyles.lightTextColor} ${utilStyles.indentedText}`}><a href="https://sites.psu.edu/academy/2023/12/03/tales-of-water-and-mountains/">Early American History</a></span>
-          </li>
-        </ul>
       </section>
-
-<section className={utilStyles.headingMd}>
-  <h2>Education</h2>
-  <div className={utilStyles.workContainer}>
-    <div className={utilStyles.workRow}>
-      <div className={utilStyles.workTitle}>MA, <a href="https://www.tc.columbia.edu">Teachers College, Columbia University</a></div>
-      <div className={utilStyles.workDate}>2024-</div>
-    </div>
-    <div className={utilStyles.workRow}>
-      <div className={utilStyles.workTitle}>BME, <a href="https://www.shc.psu.edu">Penn State Schreyer Honors College</a></div>
-      <div className={utilStyles.workDate}>2020-2024</div>
-    </div>
-    <div className={utilStyles.workRow}>
-      <div className={utilStyles.workTitle}><a href="https://www.academy.psu.edu">Presidential Leadership Academy at Penn State</a></div>
-      <div className={utilStyles.workDate}>2021-2024</div>
-    </div>
-  </div>
-  <p> <a class='button' href="https://twitter.com/dvdhutch">Twitter</a></p>
-        <p> <a class='button' href="mailto:davidmhutchinson@hotmail.com">Email</a></p>
-        <p> <a class='button' href="https://www.linkedin.com/in/dvdhutch/">LinkedIn</a></p>
-</section>
+      <section className={utilStyles.headingMd}>
+        <h3>Education</h3>
+        <div className={utilStyles.workContainer}>
+          <div className={utilStyles.workRow}>
+            <div className={utilStyles.workTitle}>MA, <a href="https://www.tc.columbia.edu">Teachers College, Columbia University</a></div>
+            <div className={utilStyles.workDate}>2024-</div>
+          </div>
+          <div className={utilStyles.workRow}>
+            <div className={utilStyles.workTitle}>BME, <a href="https://www.shc.psu.edu">Penn State Schreyer Honors College</a></div>
+            <div className={utilStyles.workDate}>2020-2024</div>
+          </div>
+          <div className={utilStyles.workRow}>
+            <div className={utilStyles.workTitle}><a href="https://www.academy.psu.edu">Presidential Leadership Academy at Penn State</a></div>
+            <div className={utilStyles.workDate}>2021-2024</div>
+          </div>
+        </div>
+        <p> <a class='button' href="https://twitter.com/dvdhutch">Twitter</a></p>
+              <p> <a class='button' href="mailto:davidmhutchinson@hotmail.com">Email</a></p>
+              <p> <a class='button' href="https://www.linkedin.com/in/dvdhutch/">LinkedIn</a></p>
+      </section>
 
 
       {/* <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
